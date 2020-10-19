deb4dfae3c3253bbd72b2ed58362d1c9f7daac99
Idifixsmurfen <62641006+Idifixsmurfen@users.noreply.github.com>
2020-10-19 15:40:05 +0200

    Svar
    Svar på övningarna

---
+++ prov-1-main/prov-1-main/1/script.js
@@ -0,0 +1,9 @@
+let myName = "Walter"
+let myAge = 16
+let isHuman = true
+
+console.log(myName)
+console.log(myAge)
+console.log(isHuman)
+
+/*if (myAge === 16 && myName === "Walter" && isHuman === true){console.log("Hej Walter")}*/
